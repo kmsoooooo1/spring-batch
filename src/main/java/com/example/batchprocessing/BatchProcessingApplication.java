9727e35df574328ba68c1419620d5b9f63356b46
mskim <kmsoooooo1@gmail.com>
2023-06-21 13:55:12 +0900

    upload project

---
+++ src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.example.batchprocessing;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingApplication {
+
+	public static void main(String[] args) {
+		System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
+	}
+
+}
